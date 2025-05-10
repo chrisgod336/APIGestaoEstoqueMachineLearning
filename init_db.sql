c53d41f9c36f777aee3b660cc15d89b13c980c9e
Christian <christiansoares336@email.com>
2025-05-10 01:07:08 -0300

    merge overwrite

--- init_db.sql
+++ init_db.sql
@@ -9,6 +9,9 @@
 -- DROP TABLE IF EXISTS tb_produto;
 -- DROP TABLE IF EXISTS tb_fornecedor;
 -- DROP TABLE IF EXISTS tb_cliente;
+-- DROP TABLE IF EXISTS tb_previsao_venda;
+-- DROP TABLE IF EXISTS tb_previsao_compra;
+-- DROP TABLE IF EXISTS tb_estoque;
 
 PRAGMA foreign_keys = ON;
 
@@ -115,21 +118,24 @@ CREATE TABLE IF NOT EXISTS tb_previsao_venda(
 	mes INTEGER NOT NULL,
 	ano INTEGER NOT NULL,
 	id_produto INTEGER NOT NULL,
-	nu quantidade INTEGER NOT NULL
+	nu quantidade INTEGER NOT NULL,
+  vr_total REAL NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS tb_previsao_compra(
 	mes INTEGER NOT NULL,
 	ano INTEGER NOT NULL,
 	id_produto INTEGER NOT NULL,
-	nu quantidade INTEGER NOT NULL
+	nu quantidade INTEGER NOT NULL,
+  vr_total REAL NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS tb_previsao_estoque(
 	mes INTEGER NOT NULL,
 	ano INTEGER NOT NULL,
 	id_produto INTEGER NOT NULL,
-	nu quantidade INTEGER NOT NULL
+	nu quantidade INTEGER NOT NULL,
+  vr_total REAL NOT NULL DEFAULT 0
 );
 
 -- --INSERTS VENDAS
